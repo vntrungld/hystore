287f8a646c1444dff4fc99b8f9fe970c61e6c9f1
trungld <vn.trungld@gmail.com>
2016-11-20 16:24:46 +0700

    Update: Developer application page

--- client/app/dev/application/application.controller.js
+++ client/app/dev/application/application.controller.js
@@ -1,4 +1,15 @@
 'use strict';
 
 export default class DevApplicationController {
+  applications: Object[];
+
+  /*@ngInject*/
+  constructor(ApplicationResource) {
+    this.applications = ApplicationResource.query();
+  }
+
+  delete(application) {
+    application.$remove();
+    this.applications.splice(this.applications.indexOf(application), 1);
+  }
 }
--- client/app/dev/application/edit/edit.controller.js
+++ client/app/dev/application/edit/edit.controller.js
@@ -1,11 +1,43 @@
 'use strict';
 
 export default class DevApplicationEditController {
-  /*@ngInject*/
-  constructor(Upload) {
-    console.log(Upload);
+  categories: Object[];
+  application: {
+    name: '',
+    archive: File,
+    icon: File,
+    feature: File,
+    screenshots: Object[],
+    description: '',
+    category: '',
+    version: ''
+  };
+  errors = {
+    other: undefined
+  };
+  message = '';
+  id = '';
+  applicationService;
+  isEdit = true;
+
+  /* @ngInject */
+  constructor($state, CategoryResource, Application, ApplicationResource) {
+    this.id = $state.params.id;
+    this.categories = CategoryResource.query();
+    this.applicationService = Application;
+
+    if(this.id === '') {
+      this.isEdit = false;
+    }
+
+    if(this.isEdit) {
+      this.application = ApplicationResource.get({ id: this.id });
+    }
   }
 
   saveApplication(form) {
+    if(form.$valid) {
+      this.applicationService.upload(this.application);
+    }
   }
 }
