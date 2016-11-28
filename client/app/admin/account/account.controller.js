8cc646f7b71fc4e7b9eb9a34fabe64104978ad48
trungld <vn.trungld@gmail.com>
2016-11-28 07:46:23 +0700

    Update: improve admin account page

--- client/app/admin/account/account.controller.js
+++ client/app/admin/account/account.controller.js
@@ -1,12 +1,50 @@
 'use strict';
 
 export default class AdminAccountController {
-  users: Object[];
+  users = [];
+  selected = [];
+  limitOptions = [10, 50, 100];
+  status = [{
+      name: 'Active',
+      value: 'active'
+    }, {
+      name: 'Inactive',
+      value: 'inactive'
+    }, {
+      name: 'Deactive',
+      value: 'deactive'
+    }, {
+      name: 'Delete',
+      value: 'delete'
+    }
+  ];
+  roles = [{
+      name: 'Administator',
+      value: 'admin'
+    }, {
+      name: 'Developer',
+      value: 'dev'
+    }, {
+      name: 'User',
+      value: 'user'
+    }
+  ];
+  query = {
+    order: 'name',
+    limit: 10,
+    page: 1
+  };
+  User;
 
   /*@ngInject*/
   constructor(User) {
     // Use the User $resource to fetch all users
     this.users = User.query();
+    this.User = User;
+  }
+
+  reload() {
+    this.users = this.User.query();
   }
 
   delete(user) {
