9f956b9993d4f726f3778b072475c0d62dc1e1bc
trungld <vn.trungld@gmail.com>
2016-11-28 07:38:33 +0700

    Update: add default category mongodb document

--- server/config/seed.js
+++ server/config/seed.js
@@ -4,41 +4,12 @@
  */
 
 'use strict';
-import Thing from '../api/thing/thing.model';
+import mongoose from 'mongoose';
 import User from '../api/user/user.model';
 import Category from '../api/category/category.model';
+import Application from '../api/application/application.model';
 
-Thing.find({}).remove()
-  .then(() => {
-    Thing.create({
-      name: 'Development Tools',
-      info: 'Integration with popular tools such as Webpack, Gulp, Babel, TypeScript, Karma, '
-            + 'Mocha, ESLint, Node Inspector, Livereload, Protractor, Pug, '
-            + 'Stylus, Sass, and Less.'
-    }, {
-      name: 'Server and Client integration',
-      info: 'Built with a powerful and fun stack: MongoDB, Express, '
-            + 'AngularJS, and Node.'
-    }, {
-      name: 'Smart Build System',
-      info: 'Build system ignores `spec` files, allowing you to keep '
-            + 'tests alongside code. Automatic injection of scripts and '
-            + 'styles into your index.html'
-    }, {
-      name: 'Modular Structure',
-      info: 'Best practice client and server structures allow for more '
-            + 'code reusability and maximum scalability'
-    }, {
-      name: 'Optimized Build',
-      info: 'Build process packs up your templates as a single JavaScript '
-            + 'payload, minifies your scripts/css/images, and rewrites asset '
-            + 'names for caching.'
-    }, {
-      name: 'Deployment Ready',
-      info: 'Easily deploy your app to Heroku or Openshift with the heroku '
-            + 'and openshift subgenerators'
-    });
-  });
+const ObjectId = mongoose.Schema.Types.ObjectId;
 
 User.find({}).remove()
   .then(() => {
@@ -85,88 +56,210 @@ User.find({}).remove()
 
 Category.find({}).remove()
   .then(() => {
-    Category.create({
+    return Category.create({
       name: 'Books',
+      slug: 'books',
       info: 'Book category'
     }, {
       name: 'Business',
+      slug: 'business',
       info: 'Business category'
     }, {
       name: 'Catalogues',
+      slug: 'catalogues',
       info: 'Catalogues category'
     }, {
       name: 'Education',
+      slug: 'education',
       info: 'Education category'
     }, {
       name: 'Entertainment',
+      slug: 'entertainment',
       info: 'Entertainment category'
     }, {
       name: 'Finance',
+      slug: 'finance',
       info: 'Finance category'
     }, {
       name: 'Food & Drink',
+      slug: 'food_and_drink',
       info: 'Food & Drink category'
     }, {
+      name: 'Action',
+      slug: 'action',
+      info: 'Game Action Category'
+    }, {
+      name: 'Adventure',
+      slug: 'adventure',
+      info: 'Game Adventure'
+    }, {
+      name: 'Arcade',
+      slug: 'arcade',
+      info: 'Game Arcade Category'
+    }, {
+      name: 'Board',
+      slug: 'board',
+      info: 'Game Board Category'
+    }, {
+      name: 'Card',
+      slug: 'card',
+      info: 'Game Card Category'
+    }, {
+      name: 'Casino',
+      slug: 'casino',
+      info: 'Game Casino Category'
+    }, {
+      name: 'Dice',
+      slug: 'dice',
+      info: 'Game Dice Category'
+    }, {
+      name: 'Education',
+      slug: 'game_education',
+      info: 'Game Education Category'
+    }, {
+      name: 'Family',
+      slug: 'family',
+      info: 'Game Family Category'
+    }, {
+      name: 'Music',
+      slug: 'game_music',
+      info: 'Game Music Category'
+    }, {
+      name: 'Puzzle',
+      slug: 'puzzle',
+      info: 'Game Puzzle Category'
+    }, {
+      name: 'Racing',
+      slug: 'racing',
+      info: 'Game Racing Category'
+    }, {
+      name: 'Role-Playing',
+      slug: 'role_playing',
+      info: 'Game Role-Playing Category'
+    }, {
+      name: 'Simulation',
+      slug: 'simulation',
+      info: 'Game Simulation Category'
+    }, {
+      name: 'Sport',
+      slug: 'sport',
+      info: 'Game Sport Category'
+    }, {
+      name: 'Strategy',
+      slug: 'strategy',
+      info: 'Game Strategy Category'
+    }, {
+      name: 'Travia',
+      slug: 'travia',
+      info: 'Game Travia Category'
+    }, {
+      name: 'Word',
+      slug: 'word',
+      info: 'Game Word Category'
+    }, {
       name: 'Games',
-      info: 'Game category'
+      slug: 'games',
+      info: 'Games Category'
     }, {
       name: 'Health & Fitness',
+      slug: 'health_and_fitness',
       info: 'Health & Fitness category'
     }, {
       name: 'Kids',
+      slug: 'kids',
       info: 'Kids category'
     }, {
       name: 'Lifestyle',
+      slug: 'lifestyle',
       info: 'Lifestyle category'
     }, {
       name: 'Magazines & Newspapers',
+      slug: 'magazines_and_newspapers',
       info: 'Magazines & Newspapers category'
     }, {
       name: 'Medical',
+      slug: 'medical',
       info: 'Medical category'
     }, {
       name: 'Music',
+      slug: 'music',
       info: 'Music category'
     }, {
       name: 'Navigation',
+      slug: 'navigation',
       info: 'Navigation category'
     }, {
       name: 'News',
+      slug: 'news',
       info: 'News category'
     }, {
       name: 'Photo & Video',
+      slug: 'photo_and_video',
       info: 'Photo & Video category'
     }, {
       name: 'Productivity',
+      slug: 'productivity',
       info: 'Productivity category'
     }, {
       name: 'Reference',
+      slug: 'reference',
       info: 'Reference category'
     }, {
       name: 'Shopping',
+      slug: 'shopping',
       info: 'Shopping category'
     }, {
       name: 'Social Networking',
+      slug: 'social_networking',
       info: 'Social Networking category'
     }, {
       name: 'Sports',
+      slug: 'sports',
       info: 'Sports category'
     }, {
       name: 'Travel',
+      slug: 'travel',
       info: 'Travel category'
     }, {
       name: 'Utilities',
+      slug: 'utilities',
       info: 'Utilities category'
     }, {
       name: 'Weather',
+      slug: 'weather',
       info: 'Weather category'
     }, {
       name: 'Deletes',
+      slug: 'deletes',
       info: 'Delete category',
       status: 'delete'
     }, {
       name: 'Deactives',
+      slug: 'deactives',
       info: 'Deactive category',
       status: 'deactive'
     });
+  })
+  .then(() => {
+    const gameChildren = [ 'action', 'adventure', 'arcade', 'board', 'card',
+      'casino', 'dice', 'game_education', 'family', 'game_music', 'puzzle',
+      'racing', 'role_playing', 'simulation', 'sport', 'strategy', 'travia', 'word' ]
+
+    Category.find({ slug: { $in: gameChildren } })
+      .then(entities => {
+        const entitiesId = entities.map(entity => entity._id);
+
+        Category.findOne({ slug: 'games' })
+          .then(gamesCategory => {
+            entities.map(entity => {
+              entity.parent = gamesCategory._id;
+              entity.save();
+            });
+
+            gamesCategory.children = entitiesId;
+            gamesCategory.save();
+          });
+      })
   });
+
+Application.find({}).remove();
