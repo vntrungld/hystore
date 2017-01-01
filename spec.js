926c2d9d804d767355b48d7ed4484dd6dfbf3091
trungld <vn.trungld@gmail.com>
2017-01-02 01:24:24 +0700

    Update: remove socket mock in spec

--- spec.js
+++ spec.js
@@ -6,7 +6,6 @@ require('babel-polyfill');
 require('angular');
 require('angular-mocks');
 require('./client/components/ui-router/ui-router.mock');
-require('./client/components/socket/socket.mock');
 
 testsContext = require.context('./client', true, /\.spec\.js$/);
 testsContext.keys().forEach(testsContext);
