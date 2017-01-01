9df1f2b85b47ccf656ff11a9fb03e8ed53b6deab
trungld <vn.trungld@gmail.com>
2017-01-02 01:17:03 +0700

    Update: client app

--- client/app/app.js
+++ client/app/app.js
@@ -9,6 +9,8 @@ import ngSanitize from 'angular-sanitize';
 import uiRouter from 'angular-ui-router';
 import ngMaterial from 'angular-material';
 import ngMessages from 'angular-messages';
+import mdDataTable from 'angular-material-data-table';
+
 // import ngValidationMatch from 'angular-validation-match';
 
 import { routeConfig } from './app.config';
@@ -21,28 +23,53 @@ import admin from './admin';
 import dev from './dev';
 import navbar from '../components/navbar/navbar.component';
 import footer from '../components/footer/footer.component';
-import main from './main/main.component';
+import main from './main';
+import applicationPage from './application';
 import constants from './app.constants';
 import util from '../components/util/util.module';
+import starRating from '../components/star-rating/star-rating.directive';
+import review from '../components/review/review.module';
 
 import './app.scss';
 
-const mdDataTable = require('angular-material-data-table');
+const ngTouch = require('angular-touch'); // eslint-disable-line
+const carousel = require('angular-carousel/dist/angular-carousel'); // eslint-disable-line
 
-angular.module('hystoreApp', [ngCookies, ngResource, ngSanitize, uiRouter,
-  ngMaterial, _Auth, Category, Application, account, admin, dev, navbar,
-  footer, main, constants, util, mdDataTable, ngMessages
+angular.module('hystoreApp', [
+  ngCookies,
+  ngResource,
+  ngSanitize,
+  uiRouter,
+  ngMaterial,
+  _Auth,
+  Category,
+  Application,
+  account,
+  admin,
+  dev,
+  navbar,
+  footer,
+  main,
+  constants,
+  util,
+  mdDataTable,
+  ngMessages,
+  applicationPage,
+  'angular-carousel',
+  starRating,
+  review,
 ])
-  .config(['$mdAriaProvider', function ($mdAriaProvider) {
-      $mdAriaProvider.disableWarnings();
+  .config(['$mdAriaProvider', $mdAriaProvider => {
+    $mdAriaProvider.disableWarnings();
   }])
   .config(routeConfig)
-  .run(function($rootScope, $location, Auth) {
+  .run(($rootScope, $location, Auth) => {
     'ngInject';
+
     // Redirect to login if route requires auth and you're not logged in
 
-    $rootScope.$on('$stateChangeStart', function(event, next) {
-      Auth.isLoggedIn(function(loggedIn) {
+    $rootScope.$on('$stateChangeStart', (event, next) => {
+      Auth.isLoggedIn(loggedIn => {
         if(next.authenticate && !loggedIn) {
           $location.path('/login');
         }
@@ -53,6 +80,6 @@ angular.module('hystoreApp', [ngCookies, ngResource, ngSanitize, uiRouter,
 angular.element(document)
   .ready(() => {
     angular.bootstrap(document, ['hystoreApp'], {
-      strictDi: true
+      strictDi: true,
     });
   });
