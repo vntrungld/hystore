7d5a27c5dd2c8514835471f8bb42d4b554dc84d5
trungld <vn.trungld@gmail.com>
2017-02-05 17:07:26 +0700

    Update: admin application page

--- client/app/admin/application/application.controller.js
+++ client/app/admin/application/application.controller.js
@@ -1,36 +1,55 @@
 'use strict';
 
 export default class AdminApplicationController {
-  applications = [];
-  selected = [];
-  limitOptions = [10, 50, 100];
-  categories = [];
-  status = [
-    {
-      name: 'Publish',
-      value: 'publish',
-    }, {
-      name: 'Unpublish',
-      value: 'unpublish',
-    }, {
-      name: 'Depublish',
-      value: 'depublish',
-    }, {
-      name: 'Block',
-      value: 'block',
-    },
-  ];
-  query = {
-    order: 'name',
-    limit: 10,
-    page: 1,
-  };
-  ApplicationResource;
-  User;
-  userId;
+  applications: Array;
+  selected: Array;
+  limitOptions: Array;
+  categories: Array;
+  status: Array;
+  query: Object;
+  Application: Object;
+  ApplicationResource: Object;
 
   /*@ngInject*/
-  constructor(ApplicationResource) {
-    this.applications = ApplicationResource.query();
+  constructor(Application, ApplicationResource, $mdToast) {
+    this.mdToast = $mdToast;
+    this.applications = ApplicationResource.query({ role: 'admin' });
+    this.Application = Application;
+    this.ApplicationResource = ApplicationResource;
+    this.limitOptions = [10, 50, 100];
+    this.categories = [];
+    this.status = [
+      {
+        name: 'Publish',
+        value: 'publish',
+      }, {
+        name: 'Unpublish',
+        value: 'unpublish',
+      }, {
+        name: 'Depublish',
+        value: 'depublish',
+      }, {
+        name: 'Block',
+        value: 'block',
+      },
+    ];
+    this.query = {
+      order: 'name',
+      limit: 10,
+      page: 1,
+    };
+  }
+
+  changeStatus(index) {
+    const self = this; // eslint-disable-line
+
+    this.applications[index].$adminPatch()
+      .then(function() {
+        self.mdToast.showSimple('Application changed');
+      });
+  }
+
+  reload() {
+    this.applications = this.ApplicationResource.query({ role: 'admin' });
   }
 }
