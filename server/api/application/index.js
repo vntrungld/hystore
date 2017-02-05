e4f71f3adc189da571e7220c7d570f9e5ca5a0f2
trungld <vn.trungld@gmail.com>
2017-02-05 16:14:28 +0700

    Update: remove file controller from api application

--- server/api/application/index.js
+++ server/api/application/index.js
@@ -2,14 +2,13 @@
 
 var express = require('express');
 var controller = require('./application.controller');
-var File = require('../file/file.controller');
 var auth = require('../../auth/auth.service');
 
 var router = express.Router();
 
 router.get('/', controller.index);
 router.get('/:slug', controller.show);
-router.post('/', auth.hasRole('dev'), File.upload, controller.create);
+router.post('/', auth.hasRole('dev'), controller.create);
 router.put('/:slug', auth.hasRole('dev'), controller.upsert);
 router.patch('/:slug', auth.hasRole('dev'), controller.patch);
 router.delete('/:slug', auth.hasRole('dev'), controller.destroy);
