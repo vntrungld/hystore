2cc48d4ada7ec29953ae955b5bd7068ae9f713e3
trungld <vn.trungld@gmail.com>
2017-02-05 17:05:50 +0700

    Update: test for application

--- server/api/application/index.spec.js
+++ server/api/application/index.spec.js
@@ -11,22 +11,8 @@ var applicationCtrlStub = {
   destroy: 'applicationCtrl.destroy'
 };
 
-var authServiceStub = {
-  hasRole(role) {
-    return 'authService.hasRole.' + role;
-  }
-}
-
-var fileCtrlStub = {
-  upload: 'fileCtrl.upload'
-}
-
 var routerStub = {
   get: sinon.spy(),
-  put: sinon.spy(),
-  patch: sinon.spy(),
-  post: sinon.spy(),
-  delete: sinon.spy()
 };
 
 // require the index with our stubbed out modules
@@ -37,8 +23,6 @@ var applicationIndex = proxyquire('./index.js', {
     }
   },
   './application.controller': applicationCtrlStub,
-  '../../auth/auth.service': authServiceStub,
-  '../file/file.controller': fileCtrlStub
 });
 
 describe('Application API Router:', function() {
@@ -54,42 +38,18 @@ describe('Application API Router:', function() {
     });
   });
 
+  describe('GET /api/applications', function() {
+    it('should route to application.controller.index', function() {
+      routerStub.get
+        .withArgs('?category=games', 'applicationCtrl.index')
+        .should.have.been.calledOnce;
+    });
+  });
+
   describe('GET /api/applications/:slug', function() {
     it('should route to application.controller.show', function() {
       routerStub.get
         .withArgs('/:slug', 'applicationCtrl.show')
-        .should.have.been.calledOnce;
-    });
-  });
-
-  describe('POST /api/applications', function() {
-    it('should route to application.controller.create', function() {
-      routerStub.post
-        .withArgs('/', 'authService.hasRole.dev', 'fileCtrl.upload', 'applicationCtrl.create')
-        .should.have.been.calledOnce;
-    });
-  });
-
-  describe('PUT /api/applications/:slug', function() {
-    it('should route to application.controller.upsert', function() {
-      routerStub.put
-        .withArgs('/:slug', 'authService.hasRole.dev', 'applicationCtrl.upsert')
-        .should.have.been.calledOnce;
-    });
-  });
-
-  describe('PATCH /api/applications/:slug', function() {
-    it('should route to application.controller.patch', function() {
-      routerStub.patch
-        .withArgs('/:slug', 'authService.hasRole.dev', 'applicationCtrl.patch')
-        .should.have.been.calledOnce;
-    });
-  });
-
-  describe('DELETE /api/applications/:slug', function() {
-    it('should route to application.controller.destroy', function() {
-      routerStub.delete
-        .withArgs('/:slug', 'authService.hasRole.dev', 'applicationCtrl.destroy')
         .should.have.been.calledOnce;
     });
   });
