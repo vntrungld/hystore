b00d69c7eb67423da0563894d701a8eaa8dac563
trungld <vn.trungld@gmail.com>
2017-02-07 17:51:18 +0700

    Fix: dev review api events to pass eslint

--- server/api/dev/review/review.events.js
+++ server/api/dev/review/review.events.js
@@ -25,7 +25,7 @@ for(var e in events) {
 
 function emitEvent(event) {
   return function(doc) {
-    ReviewEvents.emit(event + ':' + doc._id, doc);
+    ReviewEvents.emit(`${event}:${doc._id}`, doc);
     ReviewEvents.emit(event, doc);
   };
 }
