68c94fcceda43192b576f62ea7d6cd74d3253702
trungld <vn.trungld@gmail.com>
2017-02-13 09:43:12 +0700

    Update: unit test for api category

--- server/api/category/index.spec.js
+++ server/api/category/index.spec.js
@@ -5,24 +5,10 @@ var proxyquire = require('proxyquire').noPreserveCache();
 var categoryCtrlStub = {
   index: 'categoryCtrl.index',
   show: 'categoryCtrl.show',
-  create: 'categoryCtrl.create',
-  upsert: 'categoryCtrl.upsert',
-  patch: 'categoryCtrl.patch',
-  destroy: 'categoryCtrl.destroy'
 };
-
-var authServiceStub = {
-  hasRole(role) {
-    return `authService.hasRole.${role}`
-  }
-}
 
 var routerStub = {
   get: sinon.spy(),
-  put: sinon.spy(),
-  patch: sinon.spy(),
-  post: sinon.spy(),
-  delete: sinon.spy()
 };
 
 // require the index with our stubbed out modules
@@ -33,7 +19,6 @@ var categoryIndex = proxyquire('./index.js', {
     }
   },
   './category.controller': categoryCtrlStub,
-  '../../auth/auth.service': authServiceStub
 });
 
 describe('Category API Router:', function() {
@@ -53,38 +38,6 @@ describe('Category API Router:', function() {
     it('should route to category.controller.show', function() {
       routerStub.get
         .withArgs('/:slug', 'categoryCtrl.show')
-        .should.have.been.calledOnce;
-    });
-  });
-
-  describe('POST /api/categories', function() {
-    it('should route to category.controller.create', function() {
-      routerStub.post
-        .withArgs('/', 'authService.hasRole.admin', 'categoryCtrl.create')
-        .should.have.been.calledOnce;
-    });
-  });
-
-  describe('PUT /api/categories/:slug', function() {
-    it('should route to category.controller.upsert', function() {
-      routerStub.put
-        .withArgs('/:slug', 'authService.hasRole.admin', 'categoryCtrl.upsert')
-        .should.have.been.calledOnce;
-    });
-  });
-
-  describe('PATCH /api/categories/:slug', function() {
-    it('should route to category.controller.patch', function() {
-      routerStub.patch
-        .withArgs('/:slug', 'authService.hasRole.admin', 'categoryCtrl.patch')
-        .should.have.been.calledOnce;
-    });
-  });
-
-  describe('DELETE /api/categories/:slug', function() {
-    it('should route to category.controller.destroy', function() {
-      routerStub.delete
-        .withArgs('/:slug', 'authService.hasRole.admin', 'categoryCtrl.destroy')
         .should.have.been.calledOnce;
     });
   });
