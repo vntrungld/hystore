a1daa5cb59a639aa2537ee35bf63975701796ebc
trungld <vn.trungld@gmail.com>
2017-02-13 09:45:05 +0700

    Update: remove unnecessary api for api category

--- server/api/category/category.controller.js
+++ server/api/category/category.controller.js
@@ -1,20 +1,13 @@
 /**
  * Using Rails-like standard naming convention for endpoints.
  * GET     /api/categories              ->  index
- * POST    /api/categories              ->  create
  * GET     /api/categories/:id          ->  show
- * PUT     /api/categories/:id          ->  upsert
- * PATCH   /api/categories/:id          ->  patch
- * DELETE  /api/categories/:id          ->  destroy
  */
 
 'use strict';
 /* eslint prefer-reflect: 0 */
 
-import jsonpatch from 'fast-json-patch';
 import Category from './category.model';
-
-const util = require('../../utilities');
 
 const respondWithResult = (res, statusCode) => {
   statusCode = statusCode || 200;
@@ -25,26 +18,6 @@ const respondWithResult = (res, statusCode) => {
     return null;
   };
 };
-
-const patchUpdates = patches =>
-  entity => {
-    try {
-      jsonpatch.apply(entity, patches, /*validate*/ true);
-    } catch(err) {
-      return Promise.reject(err);
-    }
-    return entity.save();
-  };
-
-const removeEntity = res =>
-  entity => {
-    if(entity) {
-      return entity.remove()
-        .then(() => {
-          res.status(204).end();
-        });
-    }
-  };
 
 const handleEntityNotFound = res =>
   entity => {
@@ -73,83 +46,10 @@ export const index = (req, res) =>
 
 // Gets a single Category from the DB
 export const show = (req, res) =>
-  Category.findOne({ _id: req.params.slug })
+  Category.findOne({ slug: req.params.slug })
     .populate('children')
     .populate('parent')
     .exec()
     .then(handleEntityNotFound(res))
     .then(respondWithResult(res))
-    .catch(handleError(res));
-
-const insertChildrenIdToParent = childId =>
-  parent => {
-    if(parent) {
-      parent.children.push(childId);
-      parent.save();
-    }
-  };
-
-const updateChildren = parentId =>
-  child => {
-    if(child) {
-      if(parentId) {
-        Category.findOne({ _id: parentId })
-          .then(insertChildrenIdToParent(child.id));
-      }
-      return child;
-    }
-    return null;
-  };
-
-
-// Creates a new Category in the DB
-export const create = (req, res) => {
-  req.body.slug = util.slugify(req.body.name);
-
-  return Category.create(req.body)
-    .then(updateChildren(req.body.parent))
-    .then(respondWithResult(res, 201))
-    .catch(handleError(res));
-};
-
-// Upserts the given Category in the DB at the specified ID
-export const upsert = (req, res) => {
-  if(req.body._id) {
-    delete req.body._id;
-  }
-  return Category.findOneAndUpdate(
-    { _id: req.params.slug },
-    req.body,
-    {
-      upsert: true,
-      setDefaultsOnInsert: true,
-      runValidators: true
-    }
-  )
-    .exec()
-    .then(respondWithResult(res))
-    .catch(handleError(res));
-};
-
-// Updates an existing Category in the DB
-export const patch = (req, res) => {
-  if(req.body._id) {
-    delete req.body._id;
-    delete req.body.createdAt;
-    delete req.body.updatedAt;
-  }
-  return Category.findOne({ _id: req.params.slug })
-    .exec()
-    .then(handleEntityNotFound(res))
-    .then(patchUpdates(req.body))
-    .then(respondWithResult(res))
-    .catch(handleError(res));
-};
-
-// Deletes a Category from the DB
-export const destroy = (req, res) =>
-  Category.findOne({ _id: req.params.id })
-    .exec()
-    .then(handleEntityNotFound(res))
-    .then(removeEntity(res))
     .catch(handleError(res));
--- server/api/category/index.js
+++ server/api/category/index.js
@@ -2,15 +2,10 @@
 
 import {Router} from 'express';
 import * as controller from './category.controller';
-import * as auth from '../../auth/auth.service';
 
 const router = new Router();
 
 router.get('/', controller.index);
 router.get('/:slug', controller.show);
-router.post('/', auth.hasRole('admin'), controller.create);
-router.put('/:slug', auth.hasRole('admin'), controller.upsert);
-router.patch('/:slug', auth.hasRole('admin'), controller.patch);
-router.delete('/:slug', auth.hasRole('admin'), controller.destroy);
 
 module.exports = router;
