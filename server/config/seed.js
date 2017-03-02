b246061183157accf2664304742a44a6f253d5a4
trungld <vn.trungld@gmail.com>
2017-03-03 01:42:57 +0700

    Update: server seed

--- server/config/seed.js
+++ server/config/seed.js
@@ -18,75 +18,57 @@ let app = {};
 const gameCategoriesQuery = [
   {
     name: 'Action',
-    slug: 'action',
     info: 'Game Action Category'
   }, {
     name: 'Adventure',
-    slug: 'adventure',
     info: 'Game Adventure'
   }, {
     name: 'Arcade',
-    slug: 'arcade',
     info: 'Game Arcade Category'
   }, {
     name: 'Board',
-    slug: 'board',
     info: 'Game Board Category'
   }, {
     name: 'Card',
-    slug: 'card',
     info: 'Game Card Category'
   }, {
     name: 'Casino',
-    slug: 'casino',
     info: 'Game Casino Category'
   }, {
     name: 'Dice',
-    slug: 'dice',
     info: 'Game Dice Category'
   }, {
     name: 'Education',
-    slug: 'game_education',
     info: 'Game Education Category'
   }, {
     name: 'Family',
-    slug: 'family',
     info: 'Game Family Category'
   }, {
     name: 'Music',
-    slug: 'game_music',
     info: 'Game Music Category'
   }, {
     name: 'Puzzle',
-    slug: 'puzzle',
     info: 'Game Puzzle Category'
   }, {
     name: 'Racing',
-    slug: 'racing',
     info: 'Game Racing Category'
   }, {
     name: 'Role-Playing',
-    slug: 'role_playing',
     info: 'Game Role-Playing Category'
   }, {
     name: 'Simulation',
-    slug: 'simulation',
     info: 'Game Simulation Category'
   }, {
     name: 'Sport',
-    slug: 'sport',
     info: 'Game Sport Category'
   }, {
     name: 'Strategy',
-    slug: 'strategy',
     info: 'Game Strategy Category'
   }, {
     name: 'Travia',
-    slug: 'travia',
     info: 'Game Travia Category'
   }, {
     name: 'Word',
-    slug: 'word',
     info: 'Game Word Category'
   },
 ];
@@ -94,108 +76,82 @@ const gameCategoriesQuery = [
 const normalCategoriesQuery = [
   {
     name: 'Books',
-    slug: 'books',
     info: 'Book category'
   }, {
     name: 'Business',
-    slug: 'business',
     info: 'Business category'
   }, {
     name: 'Catalogues',
-    slug: 'catalogues',
     info: 'Catalogues category'
   }, {
     name: 'Education',
-    slug: 'education',
     info: 'Education category'
   }, {
     name: 'Entertainment',
-    slug: 'entertainment',
     info: 'Entertainment category'
   }, {
     name: 'Finance',
-    slug: 'finance',
     info: 'Finance category'
   }, {
     name: 'Food & Drink',
-    slug: 'food_and_drink',
     info: 'Food & Drink category'
   }, {
     name: 'Health & Fitness',
-    slug: 'health_and_fitness',
     info: 'Health & Fitness category'
   }, {
     name: 'Kids',
-    slug: 'kids',
     info: 'Kids category'
   }, {
     name: 'Lifestyle',
-    slug: 'lifestyle',
     info: 'Lifestyle category'
   }, {
     name: 'Magazines & Newspapers',
-    slug: 'magazines_and_newspapers',
     info: 'Magazines & Newspapers category'
   }, {
     name: 'Medical',
-    slug: 'medical',
     info: 'Medical category'
   }, {
     name: 'Music',
-    slug: 'music',
     info: 'Music category'
   }, {
     name: 'Navigation',
-    slug: 'navigation',
     info: 'Navigation category'
   }, {
     name: 'News',
-    slug: 'news',
     info: 'News category'
   }, {
     name: 'Photo & Video',
-    slug: 'photo_and_video',
     info: 'Photo & Video category'
   }, {
     name: 'Productivity',
-    slug: 'productivity',
     info: 'Productivity category'
   }, {
     name: 'Reference',
-    slug: 'reference',
     info: 'Reference category'
   }, {
     name: 'Shopping',
-    slug: 'shopping',
     info: 'Shopping category'
   }, {
     name: 'Social Networking',
-    slug: 'social_networking',
     info: 'Social Networking category'
   }, {
     name: 'Sports',
-    slug: 'sports',
     info: 'Sports category'
   }, {
     name: 'Travel',
-    slug: 'travel',
     info: 'Travel category'
   }, {
     name: 'Utilities',
-    slug: 'utilities',
     info: 'Utilities category'
   }, {
     name: 'Weather',
-    slug: 'weather',
     info: 'Weather category'
   }, {
     name: 'Deletes',
-    slug: 'deletes',
     info: 'Delete category',
     status: 'delete'
   }, {
     name: 'Deactives',
-    slug: 'deactives',
     info: 'Deactive category',
     status: 'deactive'
   }
@@ -203,12 +159,11 @@ const normalCategoriesQuery = [
 
 const gameCategoryQuery = {
   name: 'Games',
-  slug: 'games',
   info: 'Games Category'
 };
 
 function seedUser() {
-  return User.find({}).remove()
+  return User.find().remove()
     .then(function() {
       return User.create({
         provider: 'local',
@@ -277,7 +232,7 @@ function insertCategoryRelationship(category) {
 }
 
 function seedCategory() {
-  return Category.find({}).remove()
+  return Category.find().remove()
     .then(function() {
       return Category.create(normalCategoriesQuery);
     })
@@ -296,33 +251,57 @@ function seedCategory() {
 }
 
 function seedApplication() {
-  return Application.find({}).remove()
+  return Application.find().remove()
     .then(function() {
-      return Application.create({
-        author: dev._id,
-        name: 'Clash Of Clans',
-        slug: 'clash-of-clans',
-        category: gameCategory._id,
-        icon: 'https://s3-ap-southeast-1.amazonaws.com/hystore-dev/dev@dev.com/clash-of-clans/icon.png',
-        feature: 'https://s3-ap-southeast-1.amazonaws.com/hystore-dev/dev@dev.com/clash-of-clans/feature.jpg',
-        screenshots: [
-          'https://s3-ap-southeast-1.amazonaws.com/hystore-dev/dev@dev.com/clash-of-clans/screenshot-1.jpg',
-          'https://s3-ap-southeast-1.amazonaws.com/hystore-dev/dev@dev.com/clash-of-clans/screenshot-2.jpg',
-          'https://s3-ap-southeast-1.amazonaws.com/hystore-dev/dev@dev.com/clash-of-clans/screenshot-3.jpg',
-          'https://s3-ap-southeast-1.amazonaws.com/hystore-dev/dev@dev.com/clash-of-clans/screenshot-4.jpg',
-          'https://s3-ap-southeast-1.amazonaws.com/hystore-dev/dev@dev.com/clash-of-clans/screenshot-5.jpg'
-        ],
-        versions: [{
-          major: 1,
-          minor: 2,
-          maintenance: 3,
-          whatsnew: 'Merry Clashmas! • A slew of special events are in store • New levels for Heroes, Golem and more • Dragon and P.E.K.K.A are striking so fast • Discounts and boosting, it’s Clashmas at last!', // eslint-disable-line
-          archive: 'https://s3-ap-southeast-1.amazonaws.com/hystore-dev/dev@dev.com/clash-of-clans/v1.2.3/coc.zip'
-        }],
-        description: '<div><!--block-->From rage-­filled Barbarians with glorious mustaches to pyromaniac wizards, raise your own army and lead your clan to victory! Build your village to fend off raiders, battle against millions of players worldwide, and forge a powerful clan with others to destroy enemy clans.<br><br>PLEASE NOTE! Clash of Clans is free to download and play, however some game items can also be purchased for real money. If you do not want to use this feature, please set up password protection for purchases in the settings of your Google Play Store app. Also, under our Terms of Service and Privacy Policy, you must be at least 13 years of age to play or download Clash of Clans.<br><br>A network connection is also required.<br><br>FEATURES</div><ul><li><!--block-->Build your village into an unbeatable fortress&nbsp;</li><li><!--block-->Raise your own army of Barbarians, Archers, Hog Riders, Wizards, Dragons and other mighty fighters</li><li><!--block-->Battle with players worldwide and take their Trophies</li><li><!--block-->Join together with other players to form the ultimate Clan</li><li><!--block-->Fight against rival Clans in epic Clan Wars&nbsp;</li><li><!--block-->Build 20 unique units with multiple levels of upgrades</li><li><!--block-->Discover your favorite attacking army from countless combinations of troops, spells, Heroes and Clan reinforcements&nbsp;</li><li><!--block-->Defend your village with a multitude of Cannons, Towers, Mortars, Bombs, Traps and Walls</li><li><!--block-->Fight against the Goblin King in a campaign through the realm</li></ul><div><!--block--><br><br>PLAYER REVIEWS&nbsp;<br>Clash of Clans proudly announces over five million five star reviews on Google Play.<br><br><br>SUPPORT<br>Chief, are you having problems? Visit http://supercell.helpshift.com/a/clash-of-clans/ or http://supr.cl/ClashForum or contact us in game by going to Settings &gt; Help and Support.<br><br>Privacy Policy:<br>http://www.supercell.net/privacy-policy/<br><br>Terms of Service:<br>http://www.supercell.net/terms-of-service/<br><br>Parent’s Guide:<br>http://www.supercell.net/parents</div>', // eslint-disable-line
-        stars: [123, 4, 56, 7, 89],
-        status: 'publish'
-      })
+      return Application.create([
+        {
+          author: dev._id,
+          name: 'Clash Of Clans',
+          category: gameCategory._id,
+          icon: 'https://s3-ap-southeast-1.amazonaws.com/hystore-dev/dev@dev.com/clash-of-clans/icon.png',
+          feature: 'https://s3-ap-southeast-1.amazonaws.com/hystore-dev/dev@dev.com/clash-of-clans/feature.jpg',
+          screenshots: [
+            'https://s3-ap-southeast-1.amazonaws.com/hystore-dev/dev@dev.com/clash-of-clans/screenshot-1.jpg',
+            'https://s3-ap-southeast-1.amazonaws.com/hystore-dev/dev@dev.com/clash-of-clans/screenshot-2.jpg',
+            'https://s3-ap-southeast-1.amazonaws.com/hystore-dev/dev@dev.com/clash-of-clans/screenshot-3.jpg',
+            'https://s3-ap-southeast-1.amazonaws.com/hystore-dev/dev@dev.com/clash-of-clans/screenshot-4.jpg',
+            'https://s3-ap-southeast-1.amazonaws.com/hystore-dev/dev@dev.com/clash-of-clans/screenshot-5.jpg'
+          ],
+          versions: [{
+            major: 1,
+            minor: 2,
+            maintenance: 3,
+            whatsnew: 'Merry Clashmas! • A slew of special events are in store • New levels for Heroes, Golem and more • Dragon and P.E.K.K.A are striking so fast • Discounts and boosting, it’s Clashmas at last!', // eslint-disable-line
+            archive: 'https://s3-ap-southeast-1.amazonaws.com/hystore-dev/dev@dev.com/clash-of-clans/v1.2.3/coc.zip'
+          }],
+          description: '<div><!--block-->From rage-­filled Barbarians with glorious mustaches to pyromaniac wizards, raise your own army and lead your clan to victory! Build your village to fend off raiders, battle against millions of players worldwide, and forge a powerful clan with others to destroy enemy clans.<br><br>PLEASE NOTE! Clash of Clans is free to download and play, however some game items can also be purchased for real money. If you do not want to use this feature, please set up password protection for purchases in the settings of your Google Play Store app. Also, under our Terms of Service and Privacy Policy, you must be at least 13 years of age to play or download Clash of Clans.<br><br>A network connection is also required.<br><br>FEATURES</div><ul><li><!--block-->Build your village into an unbeatable fortress&nbsp;</li><li><!--block-->Raise your own army of Barbarians, Archers, Hog Riders, Wizards, Dragons and other mighty fighters</li><li><!--block-->Battle with players worldwide and take their Trophies</li><li><!--block-->Join together with other players to form the ultimate Clan</li><li><!--block-->Fight against rival Clans in epic Clan Wars&nbsp;</li><li><!--block-->Build 20 unique units with multiple levels of upgrades</li><li><!--block-->Discover your favorite attacking army from countless combinations of troops, spells, Heroes and Clan reinforcements&nbsp;</li><li><!--block-->Defend your village with a multitude of Cannons, Towers, Mortars, Bombs, Traps and Walls</li><li><!--block-->Fight against the Goblin King in a campaign through the realm</li></ul><div><!--block--><br><br>PLAYER REVIEWS&nbsp;<br>Clash of Clans proudly announces over five million five star reviews on Google Play.<br><br><br>SUPPORT<br>Chief, are you having problems? Visit http://supercell.helpshift.com/a/clash-of-clans/ or http://supr.cl/ClashForum or contact us in game by going to Settings &gt; Help and Support.<br><br>Privacy Policy:<br>http://www.supercell.net/privacy-policy/<br><br>Terms of Service:<br>http://www.supercell.net/terms-of-service/<br><br>Parent’s Guide:<br>http://www.supercell.net/parents</div>', // eslint-disable-line
+          stars: [1554358, 612380, 1376383, 3620573, 26882030],
+          status: 'publish'
+        },
+        {
+          author: dev._id,
+          name: 'Subway Surfers',
+          category: gameCategory._id,
+          icon: 'https://s3-ap-southeast-1.amazonaws.com/hystore-dev/dev@dev.com/subway-surfers/icon.png',
+          feature: 'https://s3-ap-southeast-1.amazonaws.com/hystore-dev/dev@dev.com/subway-surfers/feature.jpg',
+          screenshots: [
+            'https://s3-ap-southeast-1.amazonaws.com/hystore-dev/dev@dev.com/subway-surfers/screenshot-1.jpg',
+            'https://s3-ap-southeast-1.amazonaws.com/hystore-dev/dev@dev.com/subway-surfers/screenshot-2.jpg',
+            'https://s3-ap-southeast-1.amazonaws.com/hystore-dev/dev@dev.com/subway-surfers/screenshot-3.jpg',
+            'https://s3-ap-southeast-1.amazonaws.com/hystore-dev/dev@dev.com/subway-surfers/screenshot-4.jpg'
+          ],
+          versions: [{
+            major: 1,
+            minor: 67,
+            maintenance: 1,
+            whatsnew: '<ul><li><!--block-->Join the World Tour in mystical Arabia</li><li><!--block-->Surf through a Subway of majestic palaces and magical treasures</li><li><!--block-->Improve your Board collection with the shiny new Jewelled board</li><li><!--block-->Upgrade the Old Dusty board to soar through the Subway with a trail of dust</li><li><!--block-->Search for magic lamps high and low to earn Weekly Hunt prizes</li></ul>', // eslint-disable-line
+            archive: 'https://s3-ap-southeast-1.amazonaws.com/hystore-dev/dev@dev.com/subway-surfers/v1.2.3/coc.zip'
+          }],
+          description: '<div><!--block-->DASH as fast as you can!&nbsp;<br>DODGE the oncoming trains!&nbsp;<br><br>Help Jake, Tricky &amp; Fresh escape from the grumpy Inspector and his dog.&nbsp;<br><br></div><ul><li><!--block-->Grind trains with your cool crew!&nbsp;</li><li><!--block-->Colorful and vivid HD graphics!&nbsp;</li><li><!--block-->Hoverboard Surfing!&nbsp;</li><li><!--block-->Paint powered jetpack!&nbsp;</li><li><!--block-->Lightning fast swipe acrobatics!&nbsp;</li><li><!--block-->Challenge and help your friends!&nbsp;</li></ul><div><!--block--><br>Join the most daring chase!&nbsp;<br><br>A Universal App with HD optimized graphics.<br><br>By Kiloo and Sybo.</div>', // eslint-disable-line
+          stars: [1229001, 627228, 1316926, 2884134, 16367824],
+          status: 'publish'
+        }
+      ])
         .then(function(entity) {
           app = entity;
         });
@@ -333,12 +312,12 @@ function seedApplication() {
 }
 
 function seedReview() {
-  return Review.find({}).remove()
+  return Review.find().remove()
     .then(function() {
       return Review.create({
         from: user._id,
         to: dev._id,
-        for: app._id,
+        for: app[0]._id,
         star: 5,
         content: 'Awesome!!'
       });
