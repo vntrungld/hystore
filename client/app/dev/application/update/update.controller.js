7a4b7cc567cf783de53e7aae732edc3409616ab4
trungld <vn.trungld@gmail.com>
2017-03-05 22:58:13 +0700

    Update: dev application update page

--- client/app/dev/application/update/update.controller.js
+++ client/app/dev/application/update/update.controller.js
@@ -1,16 +1,30 @@
 'use strict';
 
 export default class DevApplicationUpdateController {
+  update: Object;
   applicationService: Object;
 
   /*@ngInject*/
-  constructor(Application) {
+  constructor(Application, $state) {
     this.applicationService = Application;
+    this.state = $state;
   }
 
   updateApplication(form) {
     if(form.$valid) {
-      this.applicationService.patch(this.update);
+      let data = {
+        op: 'add',
+        path: '/versions/1',
+        value: {
+          major: this.update.major,
+          minor: this.update.minor,
+          maintenance: this.update.maintenance,
+          whatsnew: this.update.whatsnew
+
+        },
+        archive: this.update.archive
+      };
+      this.applicationService.update(this.state.params.id, data);
     }
   }
 }
