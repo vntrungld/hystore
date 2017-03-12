8602df60ccd3932dffa2a541a3436c0587d16966
trungld <vn.trungld@gmail.com>
2017-03-12 22:20:35 +0700

    Update: dev application update page

--- client/app/dev/application/update/update.controller.js
+++ client/app/dev/application/update/update.controller.js
@@ -5,26 +5,42 @@ export default class DevApplicationUpdateController {
   applicationService: Object;
 
   /*@ngInject*/
-  constructor(Application, $state) {
+  constructor(Application, $state, ApplicationResource, $mdToast) {
     this.applicationService = Application;
     this.state = $state;
+    this.mdToast = $mdToast;
+    const that = this;
+
+    ApplicationResource.get({
+      role: 'dev',
+      id: $state.params.id
+    }).$promise.then(function(app) {
+      that.application = app;
+    });
   }
 
   updateApplication(form) {
     if(form.$valid) {
+      const that = this;
       let data = {
         op: 'add',
-        path: '/versions/1',
+        path: `/versions/${this.application.versions.length}`,
         value: {
           major: this.update.major,
           minor: this.update.minor,
           maintenance: this.update.maintenance,
           whatsnew: this.update.whatsnew
-
         },
         archive: this.update.archive
       };
-      this.applicationService.update(this.state.params.id, data);
+      this.applicationService.update(this.state.params.id, data)
+        .then(function() {
+          that.mdToast.showSimple('Application Updated');
+          that.state.go('dev.application');
+        })
+        .catch(function(err) {
+          that.mdToast.showSimple(`Error: ${err.data.message}`);
+        });
     }
   }
 }
